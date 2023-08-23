0462a156caf02fb976f02d4699bf251c129ff51b
Christopher Gabriel Ozuna Batista <srsteve1913@gmail.com>
2023-08-22 23:40:51 -0400

    Update ExpenseResponse.cs

--- Okane.Contracts/ExpenseResponse.cs
+++ Okane.Contracts/ExpenseResponse.cs
@@ -5,4 +5,5 @@ public class ExpenseResponse
     public int Id { get; set; }
     public int Amount { get; set; }
     public required string Category { get; set; }
+    public required string Description { get; set; }
 }
