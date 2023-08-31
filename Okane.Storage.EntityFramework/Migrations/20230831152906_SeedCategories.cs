f02858870ccf1a767bc618b1619dc09c03a85b17
Omar Rodriguez <omarjavier15@gmail.com>
2023-08-31 11:41:14 -0400

    31 Database Seed

---
+++ Okane.Storage.EntityFramework/Migrations/20230831152906_SeedCategories.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Okane.Storage.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DELETE FROM public.""Categories""");
+            migrationBuilder.InsertData(table: "Categories", column: "Name", value: "Food");
+            migrationBuilder.InsertData(table: "Categories", column: "Name", value: "Groceries");
+            migrationBuilder.InsertData(table: "Categories", column: "Name", value: "Taxes");
+            migrationBuilder.InsertData(table: "Categories", column: "Name", value: "Entertainment");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder) => 
+            migrationBuilder.Sql(@"DELETE * FROM public.""Categories""");
+    }
+}
--- Okane.Storage.InMemory/InMemoryCategoriesRepository.cs
+++ Okane.Storage.InMemory/InMemoryCategoriesRepository.cs
@@ -8,6 +8,17 @@ public class InMemoryCategoriesRepository : ICategoriesRepository
     private readonly List<Category> _list = new List<Category>();
     private int _nextId = 1;
 
+    public InMemoryCategoriesRepository()
+    {
+        _list.AddRange(new[]
+        {
+            new Category { Name = "Food" },
+            new Category { Name = "Groceries" },
+            new Category { Name = "Entertainment" },
+            new Category { Name = "Taxes" },
+        });
+    }
+
     public Category? ByName(string categoryName) => 
         _list.FirstOrDefault(category => category.Name == categoryName);
 
